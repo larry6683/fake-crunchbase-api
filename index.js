8a4c06888f8f66c50b485935979788365b84f3b7
Sujith Battu <72318405+larry6683@users.noreply.github.com>
2025-01-05 23:51:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Fake data
+const data = {
+    "data": {
+        "properties": {
+            "name": "Example Company",
+            "description": "This is an example company.",
+            "address": "123 Example St, Example City, EX 12345"
+        },
+        "relationships": {
+            "subsidiaries": {
+                "items": [
+                    {"properties": {"name": "Subsidiary 1"}},
+                    {"properties": {"name": "Subsidiary 2"}},
+                    {"properties": {"name": "Subsidiary 3"}}
+                ]
+            }
+        }
+    }
+};
+
+// API endpoint
+app.get('/v3.1/organizations', (req, res) => {
+    res.json(data);
+});
+
+app.listen(port, () => {
+    console.log(`Fake Crunchbase API listening at http://localhost:${port}`);
+});
